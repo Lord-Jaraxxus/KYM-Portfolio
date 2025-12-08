d34de608931efe18f11d3eae43c2a504069722d0
Lord-Jaraxxus <ym970812@naver.com>
2025-12-08 12:45:52 +0900

    Revert "Revert "feat : CharacterState 추가. 공격시 이동&상호작용 불가, 상호작용시 이동&공격 불가하도록 업데이트.""
    This reverts commit 2d9a71fd6d4e22d3bbcd0381330a854202148da0.

---
+++ Project SL/Assets/01_Project SL/Scripts/Character/InteractStateMachineBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KYM
+{
+    public class InteractStateMachineBehaviour : StateMachineBehaviour 
+    {
+        private CharacterBase linkedCharacter;
+
+        public void setCharacter(CharacterBase character) 
+        {
+            this.linkedCharacter = character;
+        }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            this.linkedCharacter.CurrentState = CharacterState.Interacct;
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            this.linkedCharacter.CurrentState = CharacterState.Idle;
+        }
+    }
+}
