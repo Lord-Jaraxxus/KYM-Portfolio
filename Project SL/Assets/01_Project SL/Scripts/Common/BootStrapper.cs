a963c20778a7795ba9c5799e3bd0943c9836b0f4
Lord-Jaraxxus <ym970812@naver.com>
2025-12-09 18:32:20 +0900

    fix : 자잘한 부분들 수정

--- Project SL/Assets/01_Project SL/Scripts/Common/BootStrapper.cs
+++ Project SL/Assets/01_Project SL/Scripts/Common/BootStrapper.cs
@@ -60,11 +60,12 @@ namespace KYM
             // Main 싱글톤 초기화 (예: 게임 전체 컨텍스트 초기화)
             Main.Singleton.Initialize();
 
-            Main.Singleton.ReloadScene(SceneType.Ingame); // 이렇게 하면 자동으로 IngameScene 스크립트의 OnStart 부를 수 있지 않나? 굳이 밑에서 UI 일일히 안켜줘도 
+            // Main.Singleton.ReloadScene(SceneType.Ingame); // 이렇게 하면 자동으로 IngameScene 스크립트의 OnStart 부를 수 있지 않나? 굳이 밑에서 UI 일일히 안켜줘도 
 
             // 필요한 초기 UI 호출 (예 : HUD 표시)
             // UIManager.Show<PlayerHUD>(UIList.PlayerHUD);
-            // UIManager.Show<CrosshairUI>(UIList.CrosshairUI);
+             UIManager.Show<GlobalUI>(UIList.GlobalUI);
+
 
             // 하고싶은 커스텀 로직을 추가하세요.
         }
--- Project SL/Assets/01_Project SL/Scripts/UI/InventoryUI.cs
+++ Project SL/Assets/01_Project SL/Scripts/UI/InventoryUI.cs
@@ -1,28 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace KYM
 {
     public class InventoryUI : UIBase
     {
-        [SerializeField] GameObject itemSlotPrefab;
+        public Gpm.Ui.InfiniteScroll infiniteScroll;
+        public GameObject listItemPrefab;
 
-        ItemSlotUI[] itemSlots;
-        Inventory inventory;
-        
+        private Dictionary<string, InfiniteUI_ListData> infiniteDataContainer = new();
 
-        private void Start()
+        private void Awake()
         {
-            inventory.OnInventoryChanged += UpdateUI;
+            listItemPrefab.SetActive(false);
+        }
+            
+        private void OnEnable()
+        {
+            if (UserDataModel.Singleton) 
+            {
+                UserDataModel.Singleton.OnInventoryUpdated += OnReceiveInventoryUpdated;
+            }
+        }
+        private void OnDisable()
+        {
+            if (UserDataModel.Singleton) 
+            {
+                UserDataModel.Singleton.OnInventoryUpdated -= OnReceiveInventoryUpdated;
+            }
         }
 
-        public void UpdateUI()
+        public override void Show()
         {
-            var items = inventory.itemDictionary.Values;
+            base.Show();
 
-            GameObject newSlot = Instantiate(itemSlotPrefab, transform);
+            Initiialize();
+        }
+
+        private void Initiialize() 
+        {
+            infiniteScroll.ClearData();
+            infiniteDataContainer.Clear();
+
+            foreach (var item in UserDataModel.Singleton.PlayerItemDto.PlayerItems)
+            {
+                InfiniteUI_ListData newData = new InfiniteUI_ListData();
+                newData.color = Random.ColorHSV();
+                newData.itemName = item.ItemID;
+                newData.icon = null; // Assign an icon if available
+                newData.itemCount = item.ItemCount; 
+
+                infiniteScroll.InsertData(newData);
+                infiniteDataContainer.Add(item.ItemID, newData);
+            }
+        }
+
+        private void OnReceiveInventoryUpdated(PlayerItemDTO.PlayerItemData changedData) 
+        {
+            bool isExistItem = infiniteDataContainer.ContainsKey(changedData.ItemID);
+
+            if (isExistItem) 
+            {
+                // TODO : 이미 존재하는 Infinite Data 이므로, 수량만 갱신하여 infinite scroll 에 반영
+                infiniteDataContainer[changedData.ItemID].itemCount = changedData.ItemCount;
+                infiniteScroll.UpdateData(infiniteDataContainer[changedData.ItemID]);
+            }
+            else 
+            {
+                // TODO : 새로운 Infinite Data 생성 후, infinite scroll 에 추가
+                InfiniteUI_ListData newData = new InfiniteUI_ListData();
+                newData.color = Random.ColorHSV();
+                newData.itemName = changedData.ItemID;
+                newData.icon = null; // Assign an icon if available
+                newData.itemCount = changedData.ItemCount;
+
+                infiniteScroll.InsertData(newData);
+                infiniteDataContainer.Add(changedData.ItemID, newData);
+            }
         }
     }
 }
