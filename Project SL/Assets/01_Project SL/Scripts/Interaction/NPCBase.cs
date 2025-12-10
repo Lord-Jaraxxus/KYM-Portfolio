f2a5909d8dd6a905146047fb89d70fbe4d15db10
Lord-Jaraxxus <ym970812@naver.com>
2025-12-10 19:02:49 +0900

    feat (NPCBase) : NPC Base 스크립트 추가 (일단은 상인 NPC용)

---
+++ Project SL/Assets/01_Project SL/Scripts/Interaction/NPCBase.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KYM
+{
+    public class NPCBase : MonoBehaviour, IInteractable
+    {
+        [SerializeField] public InteractableType Type => InteractableType.NPC_Merchant;
+        [SerializeField] private int shopID;
+
+        public Transform GetTransform()
+        {
+            return this.transform;
+        }
+
+        private void Start()
+        {
+        }
+
+        public void Interact()
+        {
+            switch (Type)
+            {
+                case InteractableType.NPC_Merchant:
+                    ShopUI shopUI = UIManager.Singleton.GetUI<ShopUI>(UIList.ShopUI); 
+                    bool isOpen = shopUI != null && shopUI.gameObject.activeSelf;
+
+                    if (!isOpen)
+                    {
+                        shopUI.shopID = shopID;
+                        UIManager.Show<ShopUI>(UIList.ShopUI);
+                    }
+                    break;
+                case InteractableType.NPC_Dialogue:
+                    // UIManager.Singleton.OpenDialogueUI();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
