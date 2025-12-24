f14949c7eeacdf16f04a67580704230e9d54c55e
Lord-Jaraxxus <ym970812@naver.com>
2025-12-24 12:53:43 +0900

    feat (ItemActionFactory) : 아이템 행동 인터페이스 생성을 위한 팩토리

---
+++ Project SL/Assets/01_Project SL/Scripts/Item/ItemActionFactory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KYM
+{
+    public static class ItemActionFactory
+    {
+        public static void Create(ItemDataSO itemDataSO) 
+        {
+
+        }
+    }
+}
