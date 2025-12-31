395b52e53bf2a981450017cf1bbbfadc3b5b37b6
Lord-Jaraxxus <ym970812@naver.com>
2025-12-31 11:21:19 +0900

    update (CharacterBase) : 캐릭터베이스 Partial로 쪼갤 준비, 일단 상호작용 관련만 간단하게 따로 빼놓음

---
+++ Project SL/Assets/01_Project SL/Scripts/Character/CharacterBase/CharacterBase.Interact.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace KYM
+{
+    public partial class CharacterBase
+    {
+        public bool CanInteract()
+        {
+            return !interactBlockedState.Contains(CurrentState); // 상호작용 가능한 상태인지 반환
+        }
+
+        public void TryInteract(InteractableType type)
+        {
+            if (interactBlockedState.Contains(CurrentState)) { return; } // 상호작용 불가 상태일 경우 Interact 함수 종료
+            CurrentState = CharacterState.Interact; // 상호작용 상태로 전환, 줍기 애니메이션 전에 해줘야 여러번 줍는 버그가 안남..
+
+            // TODO : 이 밑에서 이제 상호작용 종류를 switch문 같은걸로 나눠서 
+            switch (type)
+            {
+                case InteractableType.DropItem: // 드롭 아이템과 상호작용 했을 때
+                    Root(); // 상호작용 애니메이션 재생, 애니메이션이 끝나면 Idle 상태로 돌아감
+                    break;
+                case InteractableType.NPC_Merchant: // 상인 NPC와 상호작용 했을 때
+                    // 뭐 별로 할거 없긴 함..
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void Root()
+        {
+            animator.SetTrigger("RootTrigger");
+        }
+    }
+}
--- Project SL/Assets/01_Project SL/Scripts/Character/CharacterBase/CharacterBase.cs
+++ Project SL/Assets/01_Project SL/Scripts/Character/CharacterBase/CharacterBase.cs
@@ -16,7 +16,7 @@ namespace KYM
         Dead
     }
 
-    public class CharacterBase : MonoBehaviour, IHittable
+    public partial class CharacterBase : MonoBehaviour, IHittable
     {
         // Third Party...? 아무튼 뭐 애니메이터, 캐릭터 컨트롤러, 애니메이션 이벤트 리스너, 무기 등등 그런것들
         [SerializeField] private Animator animator;
@@ -285,32 +285,10 @@ namespace KYM
         {
             CurrentState = state;
         }
-        public bool CanInteract()
-        {
-            return !interactBlockedState.Contains(CurrentState); // 상호작용 가능한 상태인지 반환
-        }
+
         public bool CanAttack()
         {
             return !attackBlockedStates.Contains(CurrentState); // 공격 가능한 상태인지 반환
-        }
-
-        public void TryInteract(InteractableType type)
-        {
-            if (interactBlockedState.Contains(CurrentState)) { return; } // 상호작용 불가 상태일 경우 Interact 함수 종료
-            CurrentState = CharacterState.Interact; // 상호작용 상태로 전환, 줍기 애니메이션 전에 해줘야 여러번 줍는 버그가 안남..
-
-            // TODO : 이 밑에서 이제 상호작용 종류를 switch문 같은걸로 나눠서 
-            switch (type)
-            {
-                case InteractableType.DropItem: // 드롭 아이템과 상호작용 했을 때
-                    Root(); // 상호작용 애니메이션 재생, 애니메이션이 끝나면 Idle 상태로 돌아감
-                    break;
-                case InteractableType.NPC_Merchant: // 상인 NPC와 상호작용 했을 때
-                    // 뭐 별로 할거 없긴 함..
-                    break;
-                default:
-                    break;
-            }
         }
 
         public void Attack1()
