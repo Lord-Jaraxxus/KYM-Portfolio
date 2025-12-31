e3c19b9e81dc6dbce4c5c5beb60b4ea9b2aef36b
Lord-Jaraxxus <ym970812@naver.com>
2025-12-31 15:52:31 +0900

    feat (ItemSystem) : 새로운 static 클래스를 만들어서, Item Use에 대한 행위를 함수로 넣어둠. InventoryUI 뿐만 아니라, 다른 코드나 클래스에서도 활용이 가능하도록.

---
+++ Project SL/Assets/01_Project SL/Scripts/Item/ItemSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KYM
+{
+    public static class ItemSystem
+    {
+        public static void UseItem(string itemID) 
+        {
+            // TODO : 아이템 사용, 즉 아이템 갯수를 1개 줄이고 0개가 되면 인벤토리에서 없애기 + 장비면 장착, 소모품이면 효과 적용 해야함...;
+
+            ItemDataSO itemDataSO = GameDataModel.Singleton.GetItemDataSO(itemID); // ID를 통해 아이템데이터베이스에서 ItemSO를 가져옴
+            IItemAction itemAction = ItemActionFactory.Create(itemDataSO); // 아이템 액션 인스턴스 생성
+            CharacterBase playerCharacter = PlayerCharacterContext.Singleton.CurrentPlayerCharacter; // 현재 플레이어 캐릭터 가져옴
+
+            if (itemAction is IUsableItem usable) // 사용한 아이템이 소모품일 경우
+            {
+                UserDataModel.Singleton.RemoveItem(itemID, 1); // 일단 UDM에서 갯수 하나 줄임
+                // usable.Use(playerCharacter); 이건 좀 나중에 구현
+            }
+            else if (itemAction is IEquipableItem equipable) // 사용한 아이템이 장비템일 경우
+            {
+                UserDataModel.Singleton.RemoveItem(itemID, 1); // 장비템도 일단 UDM에서 갯수 하나 줄임
+                equipable.Equip(playerCharacter);
+            }
+            else // 사용 불가 아이템일 경우
+            {
+                Debug.Log("사용 불가 아이템을 사용하셨습니다!");
+            }
+        }
+    }
+}
