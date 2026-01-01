72e5a5301abf5dbfd80b7bdaba63802022b3a7f6
Lord-Jaraxxus <ym970812@naver.com>
2026-01-01 20:07:29 +0900

    feat (CharacterEquipment) : 캐릭터 장비 스크립트 추가

---
+++ Project SL/Assets/01_Project SL/Scripts/Item/CharacterEquipment.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace KYM
+{
+    public class CharacterEquipment : MonoBehaviour
+    {
+        public EquipSlotType equipSlotType; // 장비 슬롯 타입
+        public ItemDataSO itemDataSO; // 장착된 아이템 데이터SO
+
+        // 나중에 모델링 갈아끼울때를 위한 변수
+        public GameObject equipModeling; // 모델링? 프리펩?
+        public Transform equipPoint; // 장비가 장착될 위치
+        // public Rotate rotate; // 회전도 있어야 할 것 같은데
+
+        public void ChangeEquipment(ItemDataSO beforeEquipSO, ItemDataSO newEqiupSO) 
+        {
+            itemDataSO = newEqiupSO;
+
+            // TODO : 모델링 갈아끼우기, 스텟 변경?
+        }
+    }
+}
