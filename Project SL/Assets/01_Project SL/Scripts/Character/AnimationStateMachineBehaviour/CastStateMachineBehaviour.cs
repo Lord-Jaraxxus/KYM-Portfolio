fedce24ae3c9aad7ebaa7dbce84a7a7b67ece405
Lord-Jaraxxus <ym970812@naver.com>
2026-01-16 14:24:49 +0900

    feat (CastStateMachineBehaviour) : 스킬시전 애니메이션 종료시 캐릭터 상태 변환을 위해 CastStateMachineBehaviour 추가

---
+++ Project SL/Assets/01_Project SL/Scripts/Character/AnimationStateMachineBehaviour/CastStateMachineBehaviour.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KYM
+{
+    public class CastStateMachineBehaviour : StateMachineBehaviour
+    {
+        private CharacterBase linkedCharacter;
+
+        public void setCharacter(CharacterBase character)
+        {
+            this.linkedCharacter = character;
+        }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            this.linkedCharacter.SetCharacterState(CharacterState.Cast);
+        }
+
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (linkedCharacter.CurrentState == CharacterState.Cast)   // 애니메이션에서 나갈 때 현재 상태가 Cast라면 (중간에 방해받지 않았다면 (피격 등으로))
+            {
+                this.linkedCharacter.SetCharacterState(CharacterState.Idle);
+                Debug.Log("Cast ended naturally → Idle");
+            }
+            else // 중간에 방해받았다면
+            {
+                Debug.Log("Cast interrupted → state preserved");
+            }
+        }
+    }
+}
