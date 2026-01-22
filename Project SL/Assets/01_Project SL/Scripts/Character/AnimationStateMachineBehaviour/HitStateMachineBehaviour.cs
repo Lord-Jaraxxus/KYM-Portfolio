043f41b9938d8e4c34ca2abc263f2680a0454570
Lord-Jaraxxus <ym970812@naver.com>
2026-01-22 17:43:14 +0900

    fix (HitStateMachineBehaviour) : 적캐릭이 투사체 연속으로 얻어맞으면 붕쯔붕쯔 하는 버그 수정, HitStateMachineBehaviour의 OnStateExit에서도 캐릭터가 Hit상태가 아니면 Idle로 변경하지 않게 바꿈 (겸사겸사 InteractStateMachineBehaviour도)

--- Project SL/Assets/01_Project SL/Scripts/Character/AnimationStateMachineBehaviour/HitStateMachineBehaviour.cs
+++ Project SL/Assets/01_Project SL/Scripts/Character/AnimationStateMachineBehaviour/HitStateMachineBehaviour.cs
@@ -20,7 +20,15 @@ namespace KYM
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            this.linkedCharacter.SetCharacterState(CharacterState.Idle);
+            if (linkedCharacter.CurrentState == CharacterState.Hit)   // 애니메이션에서 나갈 때 현재 상태가 Hit이라면 (연속 피격 등으로 중간에 방해받지 않았다면)
+            {
+                this.linkedCharacter.SetCharacterState(CharacterState.Idle);
+                Debug.Log("Hit ended naturally → Idle");
+            }
+            else // 중간에 방해받았다면
+            {
+                Debug.Log("Hit interrupted → state preserved");
+            }
         }
     }
 }
--- Project SL/Assets/01_Project SL/Scripts/Character/AnimationStateMachineBehaviour/InteractStateMachineBehaviour.cs
+++ Project SL/Assets/01_Project SL/Scripts/Character/AnimationStateMachineBehaviour/InteractStateMachineBehaviour.cs
@@ -21,6 +21,16 @@ namespace KYM
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             this.linkedCharacter.SetCharacterState(CharacterState.Idle);
+
+            if (linkedCharacter.CurrentState == CharacterState.Interact)   // 애니메이션에서 나갈 때 현재 상태가 Interact라면 (중간에 피격 등으로 방해받지 않았다면)
+            {
+                this.linkedCharacter.SetCharacterState(CharacterState.Idle);
+                Debug.Log("Attack ended naturally → Idle");
+            }
+            else // 중간에 방해받았다면
+            {
+                Debug.Log("Attack interrupted → state preserved");
+            }
         }
     }
 }
